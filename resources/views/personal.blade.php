c1fda9be96f7d951a695dfe966b73eba526f0384
iamfahmiazmi <fahmi@moladin.com>
2022-10-29 20:05:10 +0700

    fix icon

--- resources/views/personal.blade.php
+++ resources/views/personal.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="title" content="Aliyah & Fahmi">
-    <meta name="description" content="Landing page wedding personal Aliyah & Fahmi">
-    <meta name="keywords" content="aliyah & fahmi, wedding, wedding invitation, villa lagenta">
+    <meta name="title" content="Wedding Invitation Aliyah & Fahmi">
+    <meta name="description" content="Wedding Invitation Aliyah & Fahmi">
+    <meta name="keywords" content="aliyah & fahmi, wedding, wedding invitation">
     <meta name="author" content="Wedding Fahmi & Aliyah">
     <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5">
