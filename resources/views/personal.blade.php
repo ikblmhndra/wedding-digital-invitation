14320dd1bbfdc806973b41a045fa540ae8ff0276
ikblmhndra <ikbalmahendra.96@gmail.com>
2024-03-19 11:22:14 +0700

    update title

--- resources/views/personal.blade.php
+++ resources/views/personal.blade.php
@@ -10,7 +10,7 @@
     <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5">
     <meta name="format-detection" content="telephone=no">
-    <title>Ikbal & Ila</title>
+    <title>Ila & Ikbal</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
